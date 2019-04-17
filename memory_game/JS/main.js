5b92073b59c10820a9c60396800743172322aaa0
Huda Binzaqr <huda@Hudas-MacBook-Pro.local>
2019-04-17 19:50:35 +0300

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,19 @@
+console.log("Up and running!"); 
+
+console.log("User flipped " + cardOne); 
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0]; 
+cardsInPlay.push('cardOne');
+console.log("User flipped queen"); 
+
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+
+	alert("You found a match!");
+} else 
+    alert ("Sorry, try again"); 
